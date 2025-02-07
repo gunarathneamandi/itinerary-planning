af30ff6c3dfd567011b47b09cf6d1926befd7185
Sakinu Kulathunga <sakinukulathunga@gmail.com>
2025-02-08 00:39:51 +0530

    from login to home

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,18 +1,23 @@
 import { useState } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [message, setMessage] = useState("");
+    const navigate = useNavigate();
+
 
     // Hardcoded credentials
     const USERNAME = "admin";
     const PASSWORD = "12345";
 
+
     // Login function as a constant method
     const handleLogin = () => {
         if (username === USERNAME && password === PASSWORD) {
             setMessage("Login successful!");
+            navigate('/admin/home')
         } else {
             setMessage("Invalid username or password.");
         }
